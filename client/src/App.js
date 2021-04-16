0b5b634205081f0288e65bba8a7f5ff6b94e9f09
Noname1180 <Akela.Klimov@yandex.ru>
2021-04-16 19:26:59 +0700

    create template for registration

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import {Login} from "./components/login";
+import {Register} from "./components/login";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hello, word!
+        <Login/>
       </header>
     </div>
   );
