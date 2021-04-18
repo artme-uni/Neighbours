b78b7c438b9299a619628c2aaaab6298094f11e5
Artem Kononov <artemkononov2000@gmail.com>
2021-04-18 14:59:23 +0700

    NEIG-42 add header and routing between pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Root from "./screens/Root";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hello, word!
+          <Root> </Root>
       </header>
     </div>
   );
---
+++ client/src/screens/feed/Feed.js
@@ -0,0 +1,16 @@
+import './Feed.css';
+import MainHeader from "../../components/header/MainHeader";
+
+function Feed() {
+    return (
+        <div>
+            <MainHeader/>
+
+            <div className={"feed"}>
+                <h1>Hello, it's a feed screen!</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Feed;
