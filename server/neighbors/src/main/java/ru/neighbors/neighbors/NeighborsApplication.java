7c3add2687dd3beb87c917fc420ea4b05ac4b08a
NavruzshoevDaniel <den.navruzshoev@gmail.com>
2021-04-20 13:34:11 +0700

    server backend environment init

---
+++ server/neighbors/src/main/java/ru/neighbors/neighbors/NeighborsApplication.java
@@ -0,0 +1,13 @@
+package ru.neighbors.neighbors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeighborsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeighborsApplication.class, args);
+	}
+
+}
---
+++ server/neighbors/src/test/java/ru/neighbors/neighbors/NeighborsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.neighbors.neighbors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeighborsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
