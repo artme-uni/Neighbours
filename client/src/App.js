5d4b1e449f41d12db3cf16a9b1d68c9406888bcd
Noname1180 <Akela.Klimov@yandex.ru>
2021-04-20 23:01:50 +0700

    delete trash

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Root from "./screens/Root";
+import Login from "./components/authorization/Login";
+import Registration from "./components/authorization/Registration";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <Root> </Root>
+          {/*<Root> </Root>*/}
+          {/*<Login> </Login>*/}
+          <Registration> </Registration>
       </header>
     </div>
   );
