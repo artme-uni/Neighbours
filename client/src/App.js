8c16cbba60efb9fc96a63eeb746f162e87384e73
Noname1180 <Akela.Klimov@yandex.ru>
2021-04-23 20:46:03 +0700

    add redirect from Login to registration

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <Root> </Root>
-          { /*<Login> </Login>*/ }
+          {/*<Root> </Root>*/}
+          <Login> </Login>
           { /*<Registration> </Registration>*/ }
       </header>
     </div>
--- client/src/components/authorization/Login.js
+++ client/src/components/authorization/Login.js
@@ -8,6 +8,10 @@ export default class Login extends React.Component {
     constructor(props) {
         super(props);
         this.handleClick = this.handleClick.bind(this);
+        this.state = {
+            openRegister: false,
+            Login: true,
+        };
     }
 
     handleClick() {
@@ -16,32 +20,49 @@ export default class Login extends React.Component {
 
     redirectToRegister(){
         console.log('Открываем окно регистрации');
+        this.setState({
+            openRegister: true,
+            Login: false,
+        });
     }
 
     render() {
         return (
-            <form className = {'auth-forms'}>
-                {/*<img src={loginImg}/>*/}
-                <h3>Вход</h3>
-                <div className="form-group">
-                    <label>Адрес почты</label>
-                    <input type="email" className="form-control" placeholder="Введите адрес почты" />
-                </div>
+            <div>
+                {
+                this.state.Login ?
+                    <form className={'auth-forms'}>
+                        {/*<img src={loginImg}/>*/}
+                        <h3>Вход</h3>
+                        <div className="form-group">
+                            <label>Адрес почты</label>
+                            <input type="email" className="form-control" placeholder="Введите адрес почты"/>
+                        </div>
 
-                <div className="form-group">
-                    <label>Пароль</label>
-                    <input type="password" className="form-control" placeholder="Введите пароль" />
-                </div>
+                        <div className="form-group">
+                            <label>Пароль</label>
+                            <input type="password" className="form-control" placeholder="Введите пароль"/>
+                        </div>
 
-                <button type="submit" className="btn btn-primary btn-block">Войти</button>
-                <p className="forgot-password text-right">
-                    Забыли <a href="#" onClick={() => this.handleClick()}>пароль?</a>
-                </p>
-                <p className="have-no-account text-right">
-                    Не <a href="#" onClick={()=> this.redirectToRegister()}>зарегистрированы?</a>
-                </p>
+                        <button type="submit" className="btn btn-primary btn-block">Войти</button>
+                        <p className="forgot-password text-right">
+                            Забыли <a href="#" onClick={() => this.handleClick()}>пароль?</a>
+                        </p>
+                        <p className="have-no-account text-right">
+                            Не <a href="#" onClick={() => this.redirectToRegister()}>зарегистрированы?</a>
+                        </p>
 
-            </form>
+                    </form>:
+                    null
+                }
+
+                {
+                    this.state.openRegister ?
+                        <Registration/> :
+                        null
+                }
+
+            </div>
         );
     }
 }
