760b9d7a64cc01510ea152541823a250d427a08a
NavruzshoevDaniel <den.navruzshoev@gmail.com>
2021-05-02 20:18:40 +0700

    NEIG-68 added swagger

--- server/neighbors/src/main/java/ru/neighbors/neighbors/NeighborsApplication.java
+++ server/neighbors/src/main/java/ru/neighbors/neighbors/NeighborsApplication.java
@@ -2,8 +2,10 @@ package ru.neighbors.neighbors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class NeighborsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NeighborsApplication.class, args);
